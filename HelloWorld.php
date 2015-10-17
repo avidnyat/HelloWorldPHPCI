24aadb40109484ce37264144d2d91e464d6cb59c
pMan <pman@karmicbee.com>
2015-10-17 19:34:46 +0530

    Pushing first Hello World

---
+++ HelloWorld.php
@@ -0,0 +1,12 @@
+<?php
+
+class HelloWorld {
+	public function __construct() {
+		//echo __CLASS__ . ' was instantiated!' . PHP_EOL;
+	}
+
+	static function greet() {
+		echo 'Hello, World!' . PHP_EOL . PHP_EOL ;
+	}
+}
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo '------------------------------------------' ;
+echo PHP_EOL . 'Setting up Continuous Integration for PHP!' . PHP_EOL ;
+echo '------------------------------------------' . PHP_EOL . PHP_EOL;
+
+include 'HelloWorld.php';
+HelloWorld::greet();
+
+?>
